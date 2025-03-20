fd3880773303b1a2d4ff45b3ea39be5ac1781ec8
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-20 00:08:42 -0600

    Part 3 done except lower case

--- script.js
+++ script.js
@@ -35,21 +35,23 @@
 
 let str =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
-
+console.log(`*****PART 1*****`)
 let columns = 0; //Part 2- Declare a variable that stores the number of columns in each row of data
-let twoDArray = [];  //Part 2- initializing empty parent array
+let twoDArray = []; //Part 2- initializing empty parent array
 
-let splitStr = str.split("\n");  //Part1- Separates the characters of string 'str' at every "\n" and puts the new strings into the new 'splitStr' array
+let splitStr = str.split("\n"); //Part1- Separates the characters of string 'str' at every "\n" and puts the new strings into the new 'splitStr' array
 
-splitStr.forEach((row) => {  //iterates over each string 'row' in the 'splitStr' array
-  newRow = row.split(",");  //Separates the characters of each string at every "," and puts the new strings into the new 'newRow' array
+splitStr.forEach((row) => {
+  //iterates over each string 'row' in the 'splitStr' array
+  newRow = row.split(","); //Separates the characters of each string at every "," and puts the new strings into the new 'newRow' array
   twoDArray.push(newRow); //pushing each new row to the back of the parent array
   let [cell1, cell2, cell3, cell4] = newRow; // Separates each element of the 'newRow' array and assigns them a variable name
   console.log(cell1, cell2, cell3, cell4); //Part 1- printing results of refactoring
   columns = newRow.length; //Part 2- Calculating column number based on rows
 });
-console.log(columns); //Part2- Printing results
-console.log(twoDArray) //Part2- Printing results
+console.log(`*****PART 2*****`)
+console.log(columns);
+console.log(twoDArray);
 
 // ********* PART 2 *********
 // Declare a variable that stores the number of columns in each row of data within the CSV.
@@ -59,3 +61,21 @@ console.log(twoDArray) //Part2- Printing results
 // Each row should be its own array, with individual entries for each column.
 // Each row should be stored in a parent array, with the heading row located at index 0.
 // Cache this two-dimensional array in a variable for later use.
+
+// ********* PART 3 **********
+// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+// Convert these keys to all lowercase letters for consistency. (let result = text.toLowerCase();)
+// Store these objects in an array, in the order that they were originally listed.
+// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+console.log(`******PART 3*****`)
+  
+let headers = twoDArray.splice(0, 1);
+objectArray = [];
+
+for (i = 0; i < headers[0].length; i++) {
+  // headers[0][i].toLowerCase()
+  const dataObject = headers[0].reduce((obj, key, index) => ({ ...obj, [key]: twoDArray[i][index] }), {});
+  objectArray.push(dataObject)
+  }
+
+console.log(objectArray);
